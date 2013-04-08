930381ca8920f94e7098a69e8ef639a3e60612d0
Michelle <yhzgirl@gmail.com>
2013-04-08 12:10:33 +0100

    first commit app framework set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Befogged::Application.config.secret_token = '0bb8450e769b39cad01f22d81a80d21c70879b10db5c1e29fe5bd127c0576d8d252425f02949a622389af4541fec3332822aa595a933d682e46ab0115b1d8612'
